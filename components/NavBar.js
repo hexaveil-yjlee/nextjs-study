150743a5650979ac43b8f3fb80f8c5b1c478f562
hexaveil-yjlee <yjlee@hexaveil.com>
2022-09-05 15:03:15 +0900

    feat: styled jsx

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,27 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from './NavBar.module.css'
+import styles from "./NavBar.module.css";
 export default function NavBar() {
-    const router = useRouter();
-    return <nav>
-        <Link href="/"><a className={router.pathname === "/" ? styles.active : ''}>home</a></Link>
-        <Link href="/about"><a className={router.pathname === "/about" ? styles.active : ''}>about</a></Link>
+  const router = useRouter();
+  return (
+    <nav>
+      <Link href="/">
+        <a className={router.pathname === "/" ? "active" : ""}>home</a>
+      </Link>
+      <Link href="/about">
+        <a className={router.pathname === "/about" ? "active" : ""}>about</a>
+      </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: blue;
+        }
+      `}</style>
     </nav>
+  );
 }
