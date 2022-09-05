211f838ac22e9137968c3cab3e3b37b5660ba5b7
hexaveil-yjlee <yjlee@hexaveil.com>
2022-09-05 14:40:35 +0900

    feat: nextjs 스타일(1)

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-
+import styles from './NavBar.module.css'
 export default function NavBar() {
     const router = useRouter();
     return <nav>
-        <Link href="/"><a style={{color: router.pathname === "/" ? 'red' : 'blue'}} >home</a></Link>
-        <Link href="/about"><a style={{color: router.pathname === "/about" ? 'red' : 'blue'}} >about</a></Link>
+        <Link href="/"><a className={router.pathname === "/" ? styles.active : ''}>home</a></Link>
+        <Link href="/about"><a className={router.pathname === "/about" ? styles.active : ''}>about</a></Link>
     </nav>
 }
